b4dc9c480d2c81d07e229db1cd3514b758f55575
Dragan Velkovski <draganvelkovski77@gmail.com>
2020-04-11 18:36:56 +0200

    add react router and start with favorites component

--- src/App.js
+++ src/App.js
@@ -3,14 +3,34 @@ import logo from './logo.svg';
 import './App.css';
 import Header from "./components/layout/Header";
 import MovieSearch from "./components/movies/MovieSearch";
-
+import Favorites from "./components/movies/Favorites";
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+} from "react-router-dom";
 function App() {
-  return (
+ return (
+     <Router>
     <div className="App">
       <Header/>
-        <MovieSearch/>
+        <Switch>
+            <Route path="/favorites">
+                <Favorites />
+            </Route>
+
+            <Route path="/">
+                <MovieSearch />
+            </Route>
+        </Switch>
     </div>
+     </Router>
+
+
+
   );
+
 }
 
 export default App;
---
+++ src/components/movies/Favorites.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+
+const Favorites = () => {
+    return (
+        <h2>Favorites</h2>
+    );
+};
+
+
+export default Favorites;
